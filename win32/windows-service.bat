51994f7d4f99b6fba834193bd73d1337e6b28525
Ashish Kulkarni <kulkarni.ashish@gmail.com>
2008-09-20 13:25:04 +0530

    impserve: add support for running as a windows service

---
+++ win32/windows-service.bat
@@ -0,0 +1,23 @@
+@echo off
+set /P action="Do you want to Install (I) or Uninstall (U) the service? "
+if /I '%action:~0,1%' equ 'i' goto install
+if /I '%action:~0,1%' equ 'u' goto uninstall
+echo.
+echo ERROR: an option other than I or U was chosen.
+pause
+exit /b 1
+:install
+instsrv impserve %~dp0srvany.exe
+reg add HKLM\SYSTEM\CurrentControlSet\Services\impserve /v Description /d "Local content server and Internet proxy for the 1150/1200 ebooks"
+reg add HKLM\SYSTEM\CurrentControlSet\Services\impserve\Parameters
+reg add HKLM\SYSTEM\CurrentControlSet\Services\impserve\Parameters /v Application /d %~dp0impserve.exe
+reg add HKLM\SYSTEM\CurrentControlSet\Services\impserve\Parameters /v AppParameters /d "-l %~dp0\impserve.log
+reg add HKLM\SYSTEM\CurrentControlSet\Services\impserve\Parameters /v AppDirectory /d %~dp0
+net start impserve
+pause
+exit /b 0
+:uninstall
+net stop impserve
+instsrv impserve REMOVE
+pause
+exit /b 0
